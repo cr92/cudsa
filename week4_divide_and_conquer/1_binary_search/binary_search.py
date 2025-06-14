d828069ae1ef835feec87a2f0dd25163ad2aba0a
Agni R <agni_18_roy@yahoo.com>
2025-06-14 15:05:34 +0530

    week4 binary search

---
+++ week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -0,0 +1,32 @@
+def binary_search(arr_size, arr, query):
+    start = 0
+    end = arr_size - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == query:
+            return mid
+        if arr[mid] > query:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+
+
+def bs(arr_size, arr, query_size, queries):
+    output = []
+    for i in range(query_size):
+        output.append(binary_search(arr_size, arr, queries[i]))
+    return " ".join(map(str, output))
+
+
+if __name__ == "__main__":
+    num_keys = int(input())
+    input_keys = list(map(int, input().split(" ")))
+    assert len(input_keys) == input_keys
+
+    num_queries = int(input())
+    queries = list(map(int, input().split(" ")))
+    assert len(queries) == num_queries
+
+    print(bs(num_keys, input_keys, num_queries, queries))
